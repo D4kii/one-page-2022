a6ee3d7c8de4757261ba0c3feb4ce110089b3c86
unknown <danielalinodasilvaoliveira@gmail.com>
2022-12-15 23:26:43 -0300

    começando distopia

--- ds1t-a/danielalino/lo.js
+++ ds1t-a/danielalino/lo.js
@@ -1,17 +1,15 @@
 
-const iconK = document.querySelector('icon-k');
-const iconRick = document.querySelector('icon-rick');
-const iconJoi = document.querySelector('icon-joi');
-const iconMariette = document.querySelector('icon-mariette');
-const iconLuv = document.querySelector('icon-luv');
-const iconAna = document.querySelector('icon-ana')
-const iconJoshi = document.querySelector('icon-joshi');
-const iconFreysa = document.querySelector('icon-freysa');
-const iconWallace = document.querySelector('icon-niander');
+    const iconK = document.querySelector('.icon-k');
+    const iconRick = document.querySelector('.icon-rick');
+    const iconJoi = document.querySelector('.icon-joi');
+    const iconMariette = document.querySelector('.icon-mariette');
+    const iconLuv = document.querySelector('.icon-luv');
+    const iconAna = document.querySelector('.icon-ana')
+    const iconJoshi = document.querySelector('.icon-joshi');
+    const iconFreysa = document.querySelector('.icon-freysa');
+    const iconWallace = document.querySelector('.icon-niander');
 
-
-function onRick () {
-    const container = document.getElementById('rick')
+    const containerRick = document.getElementById('rick')
     const containerJoi = document.getElementById('joi')
     const containerK = document.getElementById('k')
     const containerMariette = document.getElementById('mariette')
@@ -21,6 +19,9 @@ function onRick () {
     const containerFreysa = document.getElementById('freysa')
     const containerWallace = document.getElementById('wallace')
 
+
+function onRick () {
+    
     containerJoi.classList.remove('on')
     containerK.classList.remove ('on')
     containerMariette.classList.remove('on')
@@ -29,19 +30,10 @@ function onRick () {
     containerJoshi.classList.remove ('on')
     containerFreysa.classList.remove ('on')
     containerWallace.classList.remove ('on')
-    container.classList.add ('on')
+    containerRick.classList.add ('on')
 }
 
 function onJoi () {
-    const container = document.getElementById('joi')
-    const containerRick = document.getElementById('rick')
-    const containerK = document.getElementById('k')
-    const containerMariette = document.getElementById('mariette')
-    const containerLuv = document.getElementById('luv')
-    const containerAna = document.getElementById('ana')
-    const containerJoshi= document.getElementById('joshi')
-    const containerFreysa = document.getElementById('freysa')
-    const containerWallace = document.getElementById('wallace')
 
     containerK.classList.remove ('on')
     containerMariette.classList.remove('on')
@@ -51,8 +43,99 @@ function onJoi () {
     containerFreysa.classList.remove ('on')
     containerWallace.classList.remove ('on')
     containerRick.classList.remove('on')
-    container.classList.add ('on')
+    containerJoi.classList.add ('on')
+}
+function onMariette () {
+
+    containerK.classList.remove ('on')
+    containerJoi.classList.remove('on')
+    containerLuv.classList.remove ('on')
+    containerAna.classList.remove('on')
+    containerJoshi.classList.remove ('on')
+    containerFreysa.classList.remove ('on')
+    containerWallace.classList.remove ('on')
+    containerRick.classList.remove('on')
+    containerMariette.classList.add ('on')
+}
+function onLuv () {
+
+    containerK.classList.remove ('on')
+    containerMariette.classList.remove('on')
+    containerJoi.classList.remove ('on')
+    containerAna.classList.remove('on')
+    containerJoshi.classList.remove ('on')
+    containerFreysa.classList.remove ('on')
+    containerWallace.classList.remove ('on')
+    containerRick.classList.remove('on')
+    containerLuv.classList.add ('on')
+}
+function onAna () {
+
+    containerK.classList.remove ('on')
+    containerMariette.classList.remove('on')
+    containerLuv.classList.remove ('on')
+    containerJoi.classList.remove('on')
+    containerJoshi.classList.remove ('on')
+    containerFreysa.classList.remove ('on')
+    containerWallace.classList.remove ('on')
+    containerRick.classList.remove('on')
+    containerAna.classList.add ('on')
+}
+function onJoshi () {
+
+    containerK.classList.remove ('on')
+    containerMariette.classList.remove('on')
+    containerLuv.classList.remove ('on')
+    containerAna.classList.remove('on')
+    containerJoi.classList.remove ('on')
+    containerFreysa.classList.remove ('on')
+    containerWallace.classList.remove ('on')
+    containerRick.classList.remove('on')
+    containerJoshi.classList.add ('on')
+}
+function onFreysa () {
+
+    containerK.classList.remove ('on')
+    containerMariette.classList.remove('on')
+    containerLuv.classList.remove ('on')
+    containerAna.classList.remove('on')
+    containerJoshi.classList.remove ('on')
+    containerJoi.classList.remove ('on')
+    containerWallace.classList.remove ('on')
+    containerRick.classList.remove('on')
+    containerFreysa.classList.add ('on')
+}
+function onWallace () {
+
+    containerK.classList.remove ('on')
+    containerMariette.classList.remove('on')
+    containerLuv.classList.remove ('on')
+    containerAna.classList.remove('on')
+    containerJoshi.classList.remove ('on')
+    containerFreysa.classList.remove ('on')
+    containerJoi.classList.remove ('on')
+    containerRick.classList.remove('on')
+    containerWallace.classList.add ('on')
+}
+function onK () {
+
+    containerJoi.classList.remove ('on')
+    containerMariette.classList.remove('on')
+    containerLuv.classList.remove ('on')
+    containerAna.classList.remove('on')
+    containerJoshi.classList.remove ('on')
+    containerFreysa.classList.remove ('on')
+    containerWallace.classList.remove ('on')
+    containerRick.classList.remove('on')
+    containerK.classList.add ('on')
 }
 
 iconRick.addEventListener('click', onRick)
 iconJoi.addEventListener('click', onJoi)
+iconK.addEventListener('click', onK)
+iconMariette.addEventListener('click', onMariette)
+iconLuv.addEventListener('click', onLuv)
+iconAna.addEventListener('click', onAna)
+iconJoi.addEventListener('click', onJoshi)
+iconFreysa.addEventListener('click', onFreysa)
+iconWallace.addEventListener('click', onWallace)
